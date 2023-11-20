a2c082bd4411fbe8dc6d71739d8617801da9d6ed
Sckaletz <allan@leeloo.dk>
2023-11-20 18:01:25 +0100

    Wordle Finished

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 PLAN
 
-- Oprette grid 5x6 i HTML og CSS ✓
-- Tag input og placer i hver deres boks ✓
-- Læs fil og split ord efter hvert mellemrum (mangler funktion)
-- Vælg et random ord fra array ✓
-- Split ord i enkelte bogstaver og lav array
-- Lav nyt input (indtast ord) ✓
-- Er det et korrekt ord? Tjek med hele listen.
-- Sammenlign bogstaverne ud fra array
-- Angiv farver på de 5 pladser afhængig af, om de er korrekte eller ej
-- Gå til næste række
-- Hvis sidste række er udfyldt - "Beklager du er dum"
-- Hvis gæt er korrekt - "Tillykke"
+-   Oprette grid 5x6 i HTML og CSS ✓
+-   Tag input og placer i hver deres boks ✓
+-   Læs fil og split ord efter hvert mellemrum ✓
+-   Vælg et random ord fra array ✓
+-   Split ord i enkelte bogstaver og lav array ✓
+-   Lav nyt input (indtast ord) ✓
+-   Er det et korrekt ord? Tjek med hele listen ✓
+-   Sammenlign bogstaverne ud fra array ✓
+-   Angiv farver på de 5 pladser afhængig af, om de er korrekte eller ej ✓
+-   Gå til næste række ✓
+-   Hvis sidste række er udfyldt - "Du tabte" ✓
+-   Hvis gæt er korrekt - "Du vandt" ✓
--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,106 @@
+let words;
+let secretWord;
 let splitSecretWord;
+let validInput;
+let wonGame = false;
+
+let rows = {
+    row1: false,
+    row2: false,
+    row3: false,
+    row4: false,
+    row5: false,
+    row6: false,
+};
+
+function selectRow() {
+    for (let [key, value] of Object.entries(rows)) {
+        if (value === false) {
+            return key;
+        }
+    }
+}
 
 function inputValidation() {
-  let inputFromHTML = document.getElementById("input").value.toLowerCase();
-  let validInput = /^[a-z]+$/.test(inputFromHTML);
-  if (validInput && inputFromHTML.length === 5) {
-    return inputFromHTML;
-  } else {
-    alert("test");
-  }
+    let inputFromHTML = document.getElementById("input").value.toLowerCase();
+    let validInput = /^[a-z]+$/.test(inputFromHTML);
+
+    if (!validInput) {
+        alert("Dit input er ikke valid. Bogstaver kun.");
+    } else if (inputFromHTML.length !== 5) {
+        alert("Dit input er ikke 5 bogstaver langt.");
+    } else if (!words.includes(inputFromHTML)) {
+        alert("Dit ord er ikke i listen");
+    } else if (
+        validInput &&
+        inputFromHTML.length === 5 &&
+        words.includes(inputFromHTML)
+    ) {
+        return inputFromHTML;
+    }
 }
 
 function uiUpdater(splitSecretWord) {
-  let validInput = inputValidation();
-  let inputSplitArray = validInput.split("");
-  let rowSelector = document.getElementById("row1");
-  let count = 0;
+    validInput = inputValidation();
+    let inputSplitArray = validInput.split("");
+    let selectedRow = selectRow();
+    let rowHTMLSelector = document.getElementById(selectedRow);
+    let count = 0;
 
-  for (element of rowSelector.getElementsByTagName("div")) {
-    let currentLetterInSecretWord = splitSecretWord[count];
-    let currentLetterInGuessedWord = inputSplitArray[count];
+    for (element of rowHTMLSelector.getElementsByTagName("div")) {
+        if (wonGame) {
+            break;
+        }
+        let currentLetterInSecretWord = splitSecretWord[count];
+        let currentLetterInGuessedWord = inputSplitArray[count];
 
-    element.innerHTML = currentLetterInGuessedWord;
+        element.innerHTML = currentLetterInGuessedWord;
 
-    if (currentLetterInSecretWord === currentLetterInGuessedWord) {
-      element.style.backgroundColor = "rgb(0, 153, 0)";
-    } else if (splitSecretWord.includes(currentLetterInGuessedWord)) {
-      element.style.backgroundColor = "rgb(255, 215, 0)";
-    } else {
-      element.style.backgroundColor = "rgb(50, 50, 50)";
+        if (currentLetterInSecretWord === currentLetterInGuessedWord) {
+            element.style.backgroundColor = "rgb(0, 153, 0)";
+        } else if (splitSecretWord.includes(currentLetterInGuessedWord)) {
+            element.style.backgroundColor = "rgb(255, 215, 0)";
+        } else {
+            element.style.backgroundColor = "rgb(50, 50, 50)";
+        }
+        document.getElementById("input").value = "";
+        count++;
+    }
+    rows[selectedRow] = true;
+    count = 0;
+    checkIfWordIsCorrect();
+}
+
+function checkIfWordIsCorrect() {
+    for (let [key, value] of Object.entries(rows)) {
+        if (
+            key === "row6" &&
+            value === true &&
+            validInput !== secretWord &&
+            wonGame === false
+        ) {
+            alert("Du tabte");
+        }
     }
 
-    count++;
-  }
-  count = 0;
+    if (validInput === secretWord) {
+        wonGame = true;
+        alert("Du vandt");
+        document.getElementById("input").disabled = true;
+    }
+}
+
+function resetGame() {
+    location.reload();
 }
 
 fetch("/assets/wordle.txt")
-  .then((response) => response.text())
-  .then((data) => {
-    const words = data.split(" ");
-    const randomWord = Math.floor(Math.random() * words.length);
-    console.log(words[randomWord]);
+    .then((response) => response.text())
+    .then((data) => {
+        words = data.split(" ");
+        const randomWord = Math.floor(Math.random() * words.length);
+        //console.log(words[randomWord]);
 
-    let word = words[randomWord];
-    splitSecretWord = word.split("");
-  });
+        secretWord = words[randomWord];
+        splitSecretWord = secretWord.split("");
+    });
